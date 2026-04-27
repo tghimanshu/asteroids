871eba685e3089347455d76d8ab19c6fffe79f36
Himanshu Gohil <himanshugohil234@gmail.com>
2026-04-28 00:02:41 +0530

    Added clock and circleshape

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -22,6 +24,8 @@ def main():
         screen.fill("black")
 
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
